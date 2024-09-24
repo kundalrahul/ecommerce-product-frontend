173e3f1dc14748ae233ed490c0dc5c1053024f27
kundalrahul <kundalrahul11@gmail.com>
2024-09-24 18:25:35 -0400

    Test

--- src/config.js
+++ src/config.js
@@ -1,5 +1,10 @@
+// const config = {
+//   API_BASE_URL: "http://localhost:8080", // Update this to match your backend URL
+// };
+
 const config = {
-  API_BASE_URL: "http://localhost:8080", // Update this to match your backend URL
+  API_BASE_URL:
+    "https://product-backend-service-96497154465.us-central1.run.app", // Update this to match your backend URL
 };
 
 export default config;
